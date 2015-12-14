56ce0ddea6083298f3a09a7b832f2865f322e7c5
Athman Gude <athmangude@gmail.com>
2015-12-14 18:03:10 +0300

    searching for properties using current location

--- SearchPage.js
+++ SearchPage.js
@@ -94,6 +94,25 @@ class SearchPage extends Component {
         }
     }
 
+    onLocationPressed(response) {
+        navigator.geolocation.getCurrentPosition(
+            (location) => {
+                var search = location.coords.latitude + ', ' + location.coords.longitude;
+                this.setState({
+                    searchString: search
+                });
+
+                var query = urlForQueryAndPage('center_point', search, 1);
+                this._executeQuery(query);
+            },
+            (error) => {
+                this.setState({
+                    message: 'There was a problem with obtaining your location: ' + error
+                });
+            }
+        )
+    }
+
     render() {
 
         var spinner = this.state.isLoading ?
@@ -124,7 +143,8 @@ class SearchPage extends Component {
                 </View>
                 <TouchableHighlight
                     style={styles.button}
-                    underlayColor="#99d9f4">
+                    underlayColor="#99d9f4"
+                    onPress={this.onLocationPressed.bind(this)} >
                     <Text style={styles.buttonText}>Location</Text>
                 </TouchableHighlight>
                 <Image
