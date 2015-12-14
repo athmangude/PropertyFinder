695fbc47151b79a5738e7d42badc9cb47a18cea3
Athman Gude <athmangude@gmail.com>
2015-12-14 12:26:07 +0300

    added NavigatorIOS >modified stylesheet >removed clutter

--- index.ios.js
+++ index.ios.js
@@ -10,9 +10,10 @@ var {
   StyleSheet,
   Text,
   View,
+  NavigatorIOS
 } = React;
 
-class PropertyFinder extends React.Component {
+class HelloWorld extends React.Component {
     constructor() {
         super();
     }
@@ -24,41 +25,27 @@ class PropertyFinder extends React.Component {
     }
 }
 
-// var PropertyFinder = React.createClass({
-//   render: function() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Welcome to React Native!
-//         </Text>
-//         <Text style={styles.instructions}>
-//           To get started, edit index.ios.js
-//         </Text>
-//         <Text style={styles.instructions}>
-//           Press Cmd+R to reload,{'\n'}
-//           Cmd+D or shake for dev menu
-//         </Text>
-//       </View>
-//     );
-//   }
-// });
+class PropertyFinder extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                    title: 'Property Finder',
+                    component: HelloWorld
+                }}
+             />
+        );
+    }
+}
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
   text: {
       color: 'black',
